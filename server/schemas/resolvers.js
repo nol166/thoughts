beceba64c0cda45fbd0530e88620526b678b3ee7
John McCambridge <jmccamb@gmail.com>
2021-01-11 12:52:38 -0600

    Update resolvers, typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,23 @@ const resolvers = {
             const params = username ? { username } : {}
             return Thought.find().sort({ createdAt: -1 })
         },
+        thought: async (parent, { _id }) => {
+            Thought.findOne({ _id })
+        },
+        // get all users
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+        },
+        // get a single user
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts')
+        },
     },
 }
 
